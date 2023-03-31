4282161a4736a5fd0f6504c16679b1e902ada742
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-03-31 19:00:44 +0300

    feat: three squares flexbox

--- App.tsx
+++ App.tsx
@@ -1,38 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const handleClick = () => {
-    console.log("clicked!");
-  };
-
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button onPress={() => {}} title="Add" />
+      <View style={styles.one}>
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.two}>
+        <Text>2</Text>
+      </View>
+      <View style={styles.three}>
+        <Text>3</Text>
       </View>
     </View>
   );
 }
 
+const square = {
+  width: 100,
+  height: 100,
+};
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    display: "flex",
+    // alignItems: "center",
+    justifyContent: "center",
+    height: "30%",
     flexDirection: "row",
-    justifyContent: "space-between",
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 8,
-    padding: 8,
+  one: {
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    ...square,
+  },
+  two: {
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+    ...square,
+  },
+  three: {
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    ...square,
   },
 });
