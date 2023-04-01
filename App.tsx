b5d41af1ce73c421c37bedba2853c146ea1ad581
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-04-01 16:31:29 +0300

    fix: committing before end of course

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  GestureResponderEvent,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from "react-native";
 
 export default function App() {
-  const handleClick = () => {
-    console.log("clicked!");
+  const handleChangeText = (text: string) => {
+    console.log(text);
   };
 
+  const handleSubmit = (event: GestureResponderEvent) => {};
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button onPress={() => {}} title="Add" />
+        <TextInput
+          onChangeText={handleChangeText}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <Button onPress={handleSubmit} title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -21,18 +35,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     display: "flex",
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
